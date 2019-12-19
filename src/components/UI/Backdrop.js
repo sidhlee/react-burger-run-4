36f58c01a696ad76a2de5594201b68296bcf7888
Hayoun Lee <sidhlee@gmail.com>
2019-12-20 04:46:11 +0900

    creates Modal and Backdrop

---
+++ src/components/UI/Backdrop.js
@@ -0,0 +1,21 @@
+import React from "react";
+import styled from "styled-components";
+import PropTypes from "prop-types";
+
+const StyledBackdrop = styled.div`
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  top: 0;
+  z-index: 100;
+  background-color: rgba(0, 0, 0, 0.5);
+`;
+
+const Backdrop = props =>
+  props.show ? <StyledBackdrop onClick={props.cliked} /> : null;
+
+Backdrop.propTypes = {
+  clicked: PropTypes.func.isRequired
+};
+
+export default Backdrop;
---
+++ src/components/UI/Modal.js
@@ -0,0 +1,35 @@
+import React from "react";
+import styled from "styled-components";
+import Backdrop from "./Backdrop";
+
+const StyledModal = styled.div`
+  z-index: 500;
+  position: fixed;
+  width: 70%;
+  left: 15%;
+  top: 30%;
+  box-sizing: border-box;
+  padding: 16px;
+  background-color: white;
+  border: 1px solid var(--lighter-gray);
+  box-shadow: 1px 1px 1px black;
+  transition: all 0.3s ease-out;
+  transform: ${props =>
+    props.show ? "translateY(0)" : "translateY(-100vh)"};
+  opacity: ${props => (props.show ? 1 : 0)};
+  text-align: center;
+  @media (min-width: 600px) {
+    --width: 500px;
+    width: var(--width);
+    left: calc(50% - calc(var(--width) / 2));
+  }
+`;
+
+const Modal = props => (
+  <>
+    <Backdrop show={props.show} clicked={props.closeModal} />
+    <StyledModal {...props}>{props.children}</StyledModal>
+  </>
+);
+
+export default Modal;
