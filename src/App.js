90c27b3dc323b1e9e5ee075bc3bf63ea26961f2e
Hayoun Lee <sidhlee@gmail.com>
2019-12-22 19:15:15 +0900

    Stop styling Orders + Order
    and add them to App.js Routes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from "react";
 import Layout from "./hoc/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Checkout from "./containers/Checkout/Checkout";
+import Orders from "./containers/Orders/Orders";
 
 function App() {
   return (
     <div>
       <Layout>
-        {/* rendering with children element is recommended
+        <Switch>
+          {/* rendering with children element is recommended
         over using component or render prop */}
-        {/* <Route /> passes the routing props to children if (and only if) children is a function. */}
-        <Route path="/" exact component={BurgerBuilder} />
-        <Route path="/checkout" component={Checkout} />
+          {/* <Route /> passes the routing props to children if (and only if) children is a function. */}
+          <Route path="/" exact component={BurgerBuilder} />
+          <Route path="/checkout" component={Checkout} />
+          <Route path="/orders" component={Orders} />
+        </Switch>
       </Layout>
     </div>
   );
--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -3,11 +3,44 @@ import styled from "styled-components";
 import axios from "../../axios-orders";
 import withErrorHandler from "../../hoc/withErrorHandler";
 
-const StyledOrders = styled.div``;
+import Order from "./Order";
+
+const StyledOrders = styled.div`
+  margin-top: var(--margin-top);
+`;
 
 class Orders extends Component {
+  state = {
+    orders: [
+      {
+        ingredients: {
+          salad: 1,
+          bacon: 1,
+          cheese: 1,
+          beef: 1
+        },
+        totalPrice: 5
+      }
+    ]
+  };
+
   render() {
-    return <StyledOrders></StyledOrders>;
+    return (
+      <StyledOrders>
+        <Order
+          ingredients={this.state.orders[0].ingredients}
+          totalPrice={this.state.orders[0].totalPrice}
+        />
+        <Order
+          ingredients={this.state.orders[0].ingredients}
+          totalPrice={this.state.orders[0].totalPrice}
+        />
+        <Order
+          ingredients={this.state.orders[0].ingredients}
+          totalPrice={this.state.orders[0].totalPrice}
+        />
+      </StyledOrders>
+    );
   }
 }
 
