cf1e994334671d9f30a4d7f7ffcb3df10f9facef
Hayoun Lee <sidhlee@gmail.com>
2019-12-22 18:26:56 +0900

    Start Orders + Order

---
+++ src/containers/Orders/Orders.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+
+const StyledOrders = styled.div``;
+
+class Orders extends Component {
+  render() {
+    return <StyledOrders></StyledOrders>;
+  }
+}
+
+export default Orders;
