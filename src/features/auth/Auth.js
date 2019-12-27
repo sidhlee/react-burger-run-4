812119dafe043ea79164a08cf88ee6efb1131edc
Hayoun Lee <sidhlee@gmail.com>
2019-12-27 14:34:54 +0900

    Start adding auth

---
+++ src/features/auth/Auth.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+
+const StyledAuth = styled.div``;
+
+class Auth extends Component {
+  render() {
+    return <StyledAuth></StyledAuth>;
+  }
+}
+
+export default Auth;
