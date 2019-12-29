652e1e73942ee24b7bf376d883d4b2c63995e59b
Hayoun Lee <sidhlee@gmail.com>
2019-12-29 19:33:32 +0900

    Add tests on <NavigationItems />

---
+++ src/features/burgerBuilder/BurgerBuilder.test.js
@@ -0,0 +1,9 @@
+import BurgerBuiler from "./BurgerBuilder";
+import { configure, shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+configure({ adapter: new Adapter() });
+
+describe("<BurgerBuilder />", () => {
+  it("", () => {});
+});
--- src/features/layout/navigation/NavigationItems.test.js
+++ src/features/layout/navigation/NavigationItems.test.js
@@ -7,8 +7,43 @@ import NavigationItem from "./NavigationItem";
 configure({ adapter: new Adapter() });
 
 describe("<NavigationItems />", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<NavigationItems />);
+  });
+
   it("should render two <NavigationItem /> elements if not authenticated", () => {
-    const wrapper = shallow(<NavigationItems />);
     expect(wrapper.find(NavigationItem)).toHaveLength(2);
+  });
+
+  it("should render three <NavigationItem /> elements if authenticated", () => {
+    wrapper.setProps({ isAuthenticated: true });
+    expect(wrapper.find(NavigationItem)).toHaveLength(3);
+  });
+
+  it(`should contain <NavigationItem link="/sign-out">Sign Out</NavigationItem> if authenticated`, () => {
+    wrapper.setProps({ isAuthenticated: true });
+    expect(
+      wrapper.contains(
+        <NavigationItem link="/sign-out">Sign Out</NavigationItem>
+      )
+    ).toEqual(true);
+  });
+
+  it(`should contain <NavigationItem link="/orders">Orders</NavigationItem> if authenticated`, () => {
+    wrapper.setProps({ isAuthenticated: true });
+    expect(
+      wrapper.contains(
+        <NavigationItem link="/orders">Orders</NavigationItem>
+      )
+    ).toEqual(true);
+  });
+
+  it(`should contain <NavigationItem link="/auth">Sign In</NavigationItem> if not authenticated`, () => {
+    expect(
+      wrapper.contains(
+        <NavigationItem link="/auth">Sign In</NavigationItem>
+      )
+    ).toEqual(true);
   });
 });
