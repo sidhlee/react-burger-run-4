288ca9a8ad4e83f8eb54869f452a09ea80d05ac3
Hayoun Lee <sidhlee@gmail.com>
2019-12-30 22:57:09 +0900

    Add test on BuildControls
    TODO: Learn testing nested Components

---
+++ src/features/burgerBuilder/Burger/BuildControls.test.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { configure, shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import BuildControls from "./BuildControls";
+import OrderButton from "./OrderButton";
+
+configure({ adapter: new Adapter() });
+
+describe("<BuildControls />", () => {
+  let wrapper;
+  const ingredients = {
+    salad: 1,
+    bacon: 1,
+    cheese: 1,
+    beef: 1
+  };
+  beforeEach(() => {
+    wrapper = shallow(
+      <BuildControls ingredients={ingredients} totalPrice={9.9999} />
+    );
+  });
+
+  // TODO: Learn testing nested components
+  // it("should contain <OrderButton>Order Now</OrderButton> if authenticated", () => {
+  //   wrapper.setProps({ isAuthenticated: true });
+  //   expect(wrapper.text()).toEqual("Order Now");
+  // });
+});
