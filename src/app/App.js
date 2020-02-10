25e4bd5a015bea1e9375db400ddf24c53de9c961
Hayoun Lee <sidhlee@gmail.com>
2020-02-10 20:07:31 +0900

    Fix App to be functional component with hooks

--- src/app/App.js
+++ src/app/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import LayoutContainer from "../features/layout/LayoutContainer";
 import BurgerBuilderContainer from "../features/burgerBuilder/BurgerBuilderContainer";
 import { Route, Switch, Redirect } from "react-router-dom";
@@ -15,21 +15,22 @@ const asyncOrders = asyncComponent(() =>
   import("../features/orders/OrdersContainer")
 );
 
-class App extends Component {
-  componentDidMount() {
-    this.props.checkAuthStatus();
-  }
-  render() {
-    const routes = this.props.isAuthenticated ? (
-      <Switch>
-        {/* rendering with children element is recommended
+const App = ({ checkAuthStatus, isAuthenticated }) => {
+  useEffect(() => {
+    checkAuthStatus();
+  }); // no need to pass an empty array here
+  //
+
+  const routes = isAuthenticated ? (
+    <Switch>
+      {/* rendering with children element is recommended
           over using component or render prop */}
-        {/* <Route /> passes the routing props to children if (and only if) children is a function. */}
-        <Route path="/" exact component={BurgerBuilderContainer} />
-        <Route path="/checkout" component={asyncCheckout} />
-        <Route path="/orders" component={asyncOrders} />
-        <Route path="/sign-out" component={SignOutContainer} />
-        {/* 
+      {/* <Route /> passes the routing props to children if (and only if) children is a function. */}
+      <Route path="/" exact component={BurgerBuilderContainer} />
+      <Route path="/checkout" component={asyncCheckout} />
+      <Route path="/orders" component={asyncOrders} />
+      <Route path="/sign-out" component={SignOutContainer} />
+      {/* 
         Here, auth route cannot be manually accessed because 
         App will re-mount when Auth page is loaded manually and run auto-auth
         and if authenticated, Auth page will redirect to redirect path,
@@ -38,23 +39,22 @@ class App extends Component {
         to the checkout page after being authenticated. (If there's no
         Auth page after being authenticated, there's no redirect)
         */}
-        <Route path="/auth" component={asyncAuth} />
-        <Redirect to="/" />
-      </Switch>
-    ) : (
-      <Switch>
-        <Route path="/" exact component={BurgerBuilderContainer} />
-        <Route path="/auth" component={asyncAuth} />
-        <Redirect to="/" />
-      </Switch>
-    );
+      <Route path="/auth" component={asyncAuth} />
+      <Redirect to="/" />
+    </Switch>
+  ) : (
+    <Switch>
+      <Route path="/" exact component={BurgerBuilderContainer} />
+      <Route path="/auth" component={asyncAuth} />
+      <Redirect to="/" />
+    </Switch>
+  );
 
-    return (
-      <div>
-        <LayoutContainer>{routes}</LayoutContainer>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <LayoutContainer>{routes}</LayoutContainer>
+    </div>
+  );
+};
 
 export default App;
